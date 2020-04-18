f974a3a8c7777dffc470ead60a4bf5d62e7bed88
zaqueu lima dos santos <zaqueulima1@gmail.com>
2020-04-18 09:47:25 -0300

    Create Transactions And Categories migrations

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,40 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToOne,
+  JoinColumn,
+} from 'typeorm';
+
+import Category from './Category';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column()
   value: number;
 
+  @Column('uuid')
   category_id: string;
 
+  @OneToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  caetgory: Category;
+
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
